7bf4a4bfa73514b8c4e31987c5a9e66122aecd04
fourhtyoz <fourhtyoz@gmail.com>
2025-06-16 10:47:25 +0300

    restructured project

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -62,17 +62,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -122,7 +111,7 @@ BOOTSTRAP3 = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
